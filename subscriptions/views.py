88e54b90835510c8348d116667289a71337f6179
yasuto-dev <79271683+yasuto-dev@users.noreply.github.com>
2021-11-23 23:17:57 +0900

    Update views.py
    3rd add

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -49,7 +49,7 @@ def stripe_config(request):
 @csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
-        domain_url = 'http://localhost:8000/'
+        domain_url = 'https://django-stripe-sample.herokuapp.com/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
             checkout_session = stripe.checkout.Session.create(
